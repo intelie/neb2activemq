ed57c4ffc08c655ae0f0a179641a234fc8d9fb98
Felipe de Menezes <menezes@poli.ufrj.br>
2012-03-02 15:13:02 -0300

    mudança na lógica verificação de parâmetros da neb2ipc_handle_data() e correção dos testes unitários. limpeza do código

---
+++ neb2ipc/src/include/testing.h
@@ -0,0 +1,14 @@
+#ifndef RETURN_MACROS_H
+#define RETURN_MACROS_H
+
+#define MISSING_PARAMETER       1
+#define FIND_HOST_NULL          2
+#define NO_SVC_IN_HOST          3
+#define HOST_CHECK_N_PROCESSED  4
+#define SVC_CHECK_N_PROCESSED   5
+#define EVENT_TYPE_UNKNOWN      6
+#define MESSAGE_NOT_SENT        7
+
+extern int statusOfCurrentMessage;
+
+#endif
