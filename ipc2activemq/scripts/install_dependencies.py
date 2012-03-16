48cc4d47bd1d26489a92a6e11a2ea8e686ecb576
Felipe de Menezes <menezes@poli.ufrj.br>
2012-03-16 15:42:53 -0300

    minor changes

--- ipc2activemq/scripts/install_dependencies.py
+++ ipc2activemq/scripts/install_dependencies.py
@@ -4,7 +4,7 @@ import os
 # PYTHON PACKAGES DEPENDENCIES INSTALLATION
 
 
-packages = [{"stomp": "stomp.py-2.0.4.tar.gz"}, {"sysv_ipc": "sysv_ipc-0.6.1.tar.gz"}, {"chardet": "python2-chardet-2.0.1.tar.gz"}]
+packages = [{"stomp": "stomp.py-2.0.4"}, {"sysv_ipc": "sysv_ipc-0.6.1"}, {"chardet": "python2-chardet-2.0.1"}]
 
 
 #TODO: check version
@@ -17,21 +17,19 @@ def install(package_name, file_version):
     return 
   except:
     print "[INSTALLING %s]" % package_name
-    if os.path.isfile("/opt/intelie/neb2activemq/ipc2activemq/lib/%s" % file_version):
-      os.system('tar zxf /opt/intelie/neb2activemq/ipc2activemq/lib/%s -C /opt/intelie/neb2activemq/ipc2activemq/lib' % file_version)
-    #elif os.path.isfile("/opt/intelie/neb2activemq/ipc2activemq/lib/%s.tgz" % file_version):
-      #os.system('tar zxf /opt/intelie/neb2activemq/ipc2activemq/lib/%s.tgz -C /opt/intelie/neb2activemq/ipc2activemq/lib' % file_version)
+    if os.path.isfile("/opt/intelie/neb2activemq/ipc2activemq/lib/%s.tar.gz" % file_version):
+      os.system('tar -xzf /opt/intelie/neb2activemq/ipc2activemq/lib/%s.tar.gz -C /opt/intelie/neb2activemq/ipc2activemq/lib' % file_version)
     else:
       print "[PACKAGE NOT FOUND FOR %s]" % package_name
       print "[ABORTING.]"
       exit(-1)
     os.chdir("/opt/intelie/neb2activemq/ipc2activemq/lib/%s" % file_version)
     os.system('sudo python setup.py install')
-    os.system('sudo rm -rf /opt/intelie/neb2activemq/ipc2activemq/lib/%s' % file_version)
     os.chdir("/opt/intelie/neb2activemq/scripts")
     try:
       exec "import %s" % package_name
       print "[%s INSTALLED!]" % package_name
+      os.system('sudo rm -rf /opt/intelie/neb2activemq/ipc2activemq/lib/%s' % file_version)
     except ImportError:
       print "[ERROR INSTALLING %s]" % package_name
       exit(-1)
