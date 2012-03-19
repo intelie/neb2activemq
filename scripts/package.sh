824536bef861d7192e362893100e5724e8da7be9
Felipe de Menezes <menezes@poli.ufrj.br>
2012-03-19 18:19:09 -0300

    minor fixes

--- scripts/package.sh
+++ scripts/package.sh
@@ -23,7 +23,7 @@ rm -rf *.tar.gz
 
 cd ..
 mkdir -p neb2activemq/scripts
-cp ../scripts/install.sh neb2activemq/scripts/
+cp ../scripts/install.sh ../scripts/deploy.sh neb2activemq/scripts/
 
 cp -r neb2ipc ipc2activemq neb2activemq
 tar -czvf neb2activemq.tar.gz neb2activemq
