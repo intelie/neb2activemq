9bca15cde692b73d3c2f4919de818e3120dcd085
Felipe de Menezes <menezes@poli.ufrj.br>
2012-03-19 18:19:09 -0300

    minor changes

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+./package.sh
+ssh root@nagios 'cd /opt/intelie; rm -rf neb*'
+scp ../dist/neb2activemq.tar.gz root@nagios:/opt/intelie
+ssh root@nagios 'cd /opt/intelie; tar zxf neb2activemq.tar.gz' 
