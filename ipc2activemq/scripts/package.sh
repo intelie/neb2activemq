b22ed65c4a86de7ec5371373593409e3dc597786
Felipe de Menezes <menezes@poli.ufrj.br>
2012-04-10 13:27:43 -0300

    Makefile and scripts adapted

--- ipc2activemq/scripts/package.sh
+++ ipc2activemq/scripts/package.sh
@@ -1,4 +1,5 @@
-grep -rl "ENV=dev" ../src/nebpublisher.sh | xargs sed -i 's/ENV=dev/ENV=prod/g' 
+grep -rl "ENV=dev" ../src/nebpublisher.sh | xargs sed -i 's/ENV=dev/ENV=prod/g' || : 
+cp -R ../src/nebpublisher/conf/dev ../src/nebpublisher/conf/prod
 cd ..
 mkdir -p dist
 tar -czvf dist/ipc2activemq.tar.gz lib/* src/* scripts/install*
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 ./package.sh
 ssh root@nagios 'cd /opt/intelie; rm -rf neb*'
 scp ../dist/neb2activemq.tar.gz root@nagios:/opt/intelie
-ssh root@nagios 'cd /opt/intelie; tar zxf neb2activemq.tar.gz' 
+ssh root@nagios 'cd /opt/intelie; tar zxf neb2activemq.tar.gz; rm -rf neb2activemq.tar.gz' 
